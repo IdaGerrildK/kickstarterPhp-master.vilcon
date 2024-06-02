8f4ee4c4f89389604e54415831e1852f8fac011d
magnusvadsagerlarsen <mal009@edu.zealand.dk>
2024-06-02 22:36:43 +0200

    ændring

--- settings/init.php
+++ settings/init.php
@@ -8,10 +8,10 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED & ~E_STRICT);
 define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
 if(CONFIG_LIVE == 0){
-    $DB_SERVER = "localhost";
-    $DB_NAME = "vilcon";
-    $DB_USER = "root";
-    $DB_PASS = "root";
+    $DB_SERVER = "mysql17.unoeuro.com";
+    $DB_NAME = "magnuslarsenzealand_dk_db_vilcon";
+    $DB_USER = "magnuslarsenzealand_dk";
+    $DB_PASS = "35xdprezE9wgGRmbk4Fy";
 }else{
     $DB_SERVER = "";
     $DB_NAME = "";
